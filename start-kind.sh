68cfa7232108938429901d6625031525544eece8
Leif <leifl@kth.se>
2021-09-13 15:38:01 +0200

    added helm usage

--- start-kind.sh
+++ start-kind.sh
@@ -7,8 +7,14 @@ kind create cluster --config=cluster-2+4.yaml
 # Command is copied from https://kind.sigs.k8s.io/docs/user/ingress/
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
+# The same command as above, but using helm instead of just kubectl. This is not working since
+# it doesn't apply the kind specific patches applied by the command above. It should however
+# work fine in a 'real' (not kind) cluster.
+# helm install ingress-nginx ingress-nginx/ingress-nginx
+
 # Wait until ready to process requests.
 # Command is copied from https://kind.sigs.k8s.io/docs/user/ingress/
+# Note that when using the helm command above, namespace must be changed to default.
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
